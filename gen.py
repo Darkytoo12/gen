0e343f1dc09097df88f3b5439f1a563009644716
Darkytoo12 <64375756+Darkytoo12@users.noreply.github.com>
2020-05-31 17:58:08 +0300

    gen.py

---
+++ gen.py
@@ -0,0 +1,17 @@
+#python 2.7.15
+import time
+import os
+import sys
+import random
+for i in range (1000000):
+   print (random.randint (120000,1000085))                      
+   print (random.randint (100000,158000))
+  
+print " _____          _   "
+print " |  ___|        | | "
+print " | |__ _ __   __| | "
+print " |  __| '_ \ / _` | " 
+print " | |__| | | | (_| | "
+print " \____|_| |_|\__,_| "
+                  
+                  
